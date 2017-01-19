d8a97bc1dd710357f3225435565bf4d54fcc9234
Claus Zinn <claus.zinn@uni-tuebingen.de>
2017-01-19 12:57:44 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# SfS-specific conversion from CMDI to MARC-21
+
+Code Repository for metadata conversion from SfS-specific CMDI profiles to MARC-21
+
+For more information, please contact claus.zinn@uni-tuebingen.de
+
+# Website
+
+The conversion from SfS-specific CMDI to MARC21 is currently being served at (VPN access required):
+
+```http://weblicht.sfs.uni-tuebingen.de/converter/NaLiDa2Marc/ ```
+
+# Status
+
+The software has no official release yet. The software has experimental support for CMDI v1.2.
+
+NaLiDa2Marc exploits context-specific information from SfS-based CMDI profiles to minimize information loss.
+For a general transformation from CMDI to MARC-21 (with more information loss), please consult Cmdi2Marc. 
+
+# Publications
+
+Zinn, Claus, Thorsten Trippel, Steve Kaminski, und Emanuel Dima (2015): "Crosswalking from CMDI to Dublin Core and MARC 21". In: Proceedings of the 10th International Conference on Language Resources and Evaluation (LREC-2016).
+
+
+
---
+++ src/main/java/de/tuebingen/uni/sfs/nalida2marc/CMDICast.java
@@ -0,0 +1,107 @@
+package de.tuebingen.uni.sfs.nalida2marc;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URL;
+import java.security.AccessControlException;
+import java.util.Iterator;
+import java.util.Properties;
+import javax.ws.rs.core.MediaType;
+import javax.xml.XMLConstants;
+import javax.xml.namespace.NamespaceContext;
+import javax.xml.namespace.QName;
+import javax.xml.transform.OutputKeys;
+import javax.xml.xquery.XQConnection;
+import javax.xml.xquery.XQDataSource;
+import javax.xml.xquery.XQPreparedExpression;
+import javax.xml.xquery.XQResultSequence;
+import net.sf.saxon.xqj.SaxonXQDataSource;
+import org.apache.commons.io.FilenameUtils;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.stream.StreamSource;
+import org.apache.commons.io.IOUtils;
+
+public class CMDICast {
+
+	public static void main(String[] args) throws Exception {
+		test();
+	}
+
+	public static void test() throws Exception {
+		File result = castFile("annotated_english_gigaword.cmdi");
+		System.out.println("first done");
+		castFile("annotated_english_gigaword.cmdi");
+		System.out.println("second done");
+		castFile("annotated_english_gigaword.cmdi");
+		System.out.println("third done");
+	}
+
+	public static File castFile(String cmdifilename) throws Exception {
+		return castFile(new File(cmdifilename));
+	}
+
+	public static InputStream getInputStream(String filename) {
+		try {
+			return new FileInputStream(filename);
+		} catch (FileNotFoundException | AccessControlException xc) { //ignore
+		}
+		try {
+			return new FileInputStream("src/main/webapp/" + filename);
+		} catch (FileNotFoundException | AccessControlException xc) { //ignore
+		}
+
+		ClassLoader cl = Thread.currentThread().getContextClassLoader();
+		InputStream is = null;
+		try {
+			is = cl.getResourceAsStream(filename);
+		} catch (AccessControlException xc) { //ignore
+			xc.printStackTrace();
+		}
+		return (is != null) ? is : cl.getResourceAsStream("../../" + filename);
+	}
+
+	public static File castFile(File dcfile) throws Exception {
+
+	    // standard call to xslt
+	    System.setProperty("javax.xml.transform.TransformerFactory",
+			       "net.sf.saxon.TransformerFactoryImpl");
+	    
+	    String clean_xsl = "CMDI2CMDI.xsl";
+	    InputStream clean_xsl_stream = getInputStream( clean_xsl );
+	    
+            String foo_xsl = "NaLiDa2MARC21.xsl";
+	    InputStream foo_xsl_stream = getInputStream( foo_xsl );
+            
+	    String name = FilenameUtils.removeExtension(dcfile.getName());
+            File intermediate = new File(dcfile.getParent(), name + ".clean.xml");
+            File output = new File(dcfile.getParent(), name + ".marc21.xml");
+
+            
+	    BufferedOutputStream intermediateStream = new BufferedOutputStream(new FileOutputStream(intermediate));
+            BufferedOutputStream outputStream = new BufferedOutputStream(new FileOutputStream(output));
+
+            TransformerFactory tfactoryClean = TransformerFactory.newInstance();
+	    Transformer transformerClean = tfactoryClean.newTransformer(new StreamSource( clean_xsl_stream )); 
+	    transformerClean.transform(new StreamSource( dcfile ), 
+				  new StreamResult( intermediateStream ));
+	    
+	    TransformerFactory tfactory = TransformerFactory.newInstance();
+	    Transformer transformer = tfactory.newTransformer(new StreamSource( foo_xsl_stream )); // new File(foo_xsl)
+	    transformer.transform(new StreamSource( intermediate ), 
+				  new StreamResult( outputStream ));
+	    
+	    return output;
+	}
+}
